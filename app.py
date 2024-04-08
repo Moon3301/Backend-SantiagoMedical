8be01a8cd83001e35bb94f368e7006feb2b45fc8
Moon3301 <carlos_db_16@live.cl>
2024-04-08 18:35:42 -0400

    commit

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
-from flask import Flask
+from flask import Flask, request, jsonify, make_response
+from werkzeug.security import check_password_hash, generate_password_hash
+from flask_jwt_extended import JWTManager, jwt_required, create_access_token, get_jwt_identity, jwt_refresh_token_required, create_refresh_token, get_jwt
 
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = 'your-secret-key'
+app.config['JWT_SECRET_KEY'] = 'jwt-secret-string'
+app.config['JWT_BLACKLIST_ENABLED'] = True
+app.config['JWT_BLACKLIST_TOKEN_CHECKS'] = ['access', 'refresh']
+
+jwt = JWTManager(app)
 
 @app.route("/")
 def hello_world():
@@ -11,3 +19,6 @@ def test():
     print('cascsac')
 
     return 0
+
+if __name__ == "__main__":
+    app.run()
